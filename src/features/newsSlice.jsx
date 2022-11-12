9243513561e6a4f47d3edc0962c8a0210bc6c322
qxan <mehmet.enfa@gmail.com>
2022-11-12 14:12:19 +0300

    loading and error

--- src/features/newsSlice.jsx
+++ src/features/newsSlice.jsx
@@ -7,16 +7,30 @@ const initialState = {
   error: false,
 };
 
-export const getNews = createAsyncThunk("getNews", async () => {
-  const API_KEY = "0385f490e28440548f50015810157970";
-  const url = `https://newsapi.org/v2/top-headlines?country=tr&apiKey=${API_KEY}`;
-  try {
-    const { data } = await axios(url);
-    return data.articles;
-  } catch (error) {
-    console.log(error);
+//? State'lerin API gibi async kaynaklardan gelen verilere gore guncellenmesi gerekebilir.
+//? Ancak boyle bir durumda async islem tamamlandiktan sonra state guncellenmelidir.
+//? Gonderilen api istegi ile dogrudan state guncellememelidir.
+//? Islemin tamamlanmasi ile gelen veriye gore state'in guncellenemsini saglamak
+//? adina bir arabirim kullanilmaktadir.
+//? Bu arabirim middleware denilir.Redux-Toolkit, default olarak Thunk kullanmaktadir.
+//! Thunk'ın amaci reducers'a islenmis sonuclari gondermeden once gecikmeli asenkron ismlerinin yurutulmesini saglamaktir.
+
+export const getNews = createAsyncThunk(
+  "getNews", //! action types
+
+  async (thunkAPI, { rejectWithValue }) => {
+    //! asyn callback function
+    const API_KEY = "0385f490e28440548f50015810157970";
+    const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`;
+    try {
+      const { data } = await axios(url);
+      return data.articles;
+    } catch (error) {
+      console.log(error);
+      return rejectWithValue("Something went wrong");
+    }
   }
-});
+);
 
 const newsSlice = createSlice({
   name: "news",
@@ -28,16 +42,16 @@ const newsSlice = createSlice({
   },
   extraReducers: (builder) => {
     builder
-      .addCase(getnews.pending, (state, actions) => {
+      .addCase(getNews.pending, (state) => {
         state.loading = true;
       })
       .addCase(getNews.fulfilled, (state, { payload }) => {
         state.newsList = payload;
-        loading = false;
+        state.loading = false;
       })
-      .addCase(getNews.rejected, (state) => {
-        state.newsList = payload;
-        state.error = false;
+      .addCase(getNews.rejected, (state, { payload }) => {
+        state.loading = false;
+        state.error = payload;
       });
   },
 });
